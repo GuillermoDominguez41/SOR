e14b41dbdb850eb0092655a987d15d427723387f
GuillermoDominguez41 <83877647+GuillermoDominguez41@users.noreply.github.com>
2023-03-14 21:21:10 -0300

    Actualización de funciones y sintaxis

--- ej1.sh
+++ ej1.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-usuario=$1
-echo "Hola $usuario" 
+function Error(){
+   echo "Error, no se ingreso ningun parametro"
+}
+
+if test $# -lt 1
+then 
+   Error
+else
+   usuario=$1
+   echo "Hola $usuario" 
+fi
 exit
 
